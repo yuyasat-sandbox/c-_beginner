d4faec68832637defa8fd2636333fa3619e2ef67
yuyasat <takiyour@gmail.com>
2023-10-15 21:48:23 +0900

    initial commit

---
+++ FirstProject/Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+public class Human
+{
+    private int age;
+    public void setAge(int age)
+    {
+        this.age = age;
+    }
+
+    public int getAge()
+    {
+        return this.age;
+    }
+}
+
+public class Program
+{
+    public static void Main(String[] args)
+    {
+        Human yamada = new Human();
+        yamada.setAge(10);
+
+        Console.WriteLine($"山田の年齢：{yamada.getAge()}");
+    }
+}
